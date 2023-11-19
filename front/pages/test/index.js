c76b719c9ebadc7bcffd48a67b215065639817fd
Kaanayden <kaanayden15@gmail.com>
2023-11-19 06:59:14 +0300

    Auto stash before merge of "Integration" and "origin/Integration"

--- front/pages/test/index.js
+++ front/pages/test/index.js
@@ -2,12 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { decryptMessage, encryptMessage } from '../../src/utils/encryption';
 import { getTokens } from '../../src/utils/nftHooks';
 import { useAccount } from 'wagmi'
+import addresses from '../../src/utils/abis/addresses.json'
+import semaphoreAbi from '../../src/utils/abis/Semaphore.json'
+import { signMessage, writeContract } from '@wagmi/core'
+import { Identity } from "@semaphore-protocol/identity"
+import { semaphore } from '../../src/utils/semaphore';
 
 export default function Test({params}) {
 
 const [message, setMessage] = useState();
 const [encrypted, setEncrypted] = useState();
 const [tokens, setTokens] = useState();
+const [text, setText] = useState();
 
 
 const { address, isConnecting, isDisconnected } = useAccount()
@@ -33,15 +39,47 @@ const handleEncrypt = async() => {
   setEncrypted( encryptMessage(message) );
 }
 
+
+
 const handleDecrypt = async( ) => {
 
   setEncrypted( decryptMessage( encrypted))
 }
+
+const handleClick = async () => {
+  const messageToSign = address+"zkHub";
+  let signedMessage = await signMessage({message: messageToSign});
+  const identity = new Identity(signedMessage);
+  console.log("identity", identity)
+
+  const { hash } = await writeContract({
+    address: addresses.Semaphore,
+    abi: semaphoreAbi,
+    functionName: 'addMember',
+    args: [0, identity.commitment],
+  });
+  console.log("hash", hash)
+  
+}
+
+const handleCreate = async () => {
+
+  const { hash } = await writeContract({
+    address: addresses.Semaphore,
+    abi: semaphoreAbi,
+    functionName: 'createGroup',
+    args: [0, 20, address],
+  });
+  console.log("hash", hash)
+  
+}
+
 getTokens(address)
   return (
 <>
-
-
+<button onClick={handleClick}>Test</button>
+<button onClick={handleCreate}>Create</button>
+<h1>{text}</h1>
 </>
   );
 }
